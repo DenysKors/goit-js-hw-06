00500ba09fcf9738ed7e6dcc013185eabc8471af
DenysKors <105918404+DenysKors@users.noreply.github.com>
2022-10-02 16:37:53 +0300

    Task Two

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,22 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+function addIngredients() { 
+  const ingredientsArray = [];
+  const ulRef = document.querySelector("#ingredients");
+
+  ingredients.forEach(ingredient => {
+    const createLi = document.createElement("li");
+
+    createLi.textContent = `${ingredient}`;
+    createLi.classList.add("item");
+    ingredientsArray.push(createLi)
+    console.log(ingredientsArray)
+  });
+
+ulRef.append(...ingredientsArray)
+}
+
+addIngredients()
