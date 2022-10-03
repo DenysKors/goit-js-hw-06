92e70c3516a4c6cbef041563ac1cbc64a4f21c47
DenysKors <105918404+DenysKors@users.noreply.github.com>
2022-10-03 19:21:22 +0300

    Task Eight

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const formRef = document.querySelector('.login-form');
 
+formRef.addEventListener('submit', onBtnClick);
+
+function onBtnClick(event) { 
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === '' || password.value === '') {
+        return alert('Пожалуйста заполните поля Email и Password')
+    }
+
+    const formData = {
+        email: email.value,
+        password: password.value
+    }
+
+    console.log(formData)
+    
+    event.currentTarget.reset();
+}
