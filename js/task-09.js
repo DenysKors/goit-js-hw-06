ec2b42be62569343eee8d51200f2f25a31f6e3d4
DenysKors <105918404+DenysKors@users.noreply.github.com>
2022-10-04 11:43:46 +0300

    Task Nine

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const bodyRef = document.querySelector('body');
+const changeColorBtnRef = document.querySelector('.change-color');
+const colorNameRef = document.querySelector('.color');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+changeColorBtnRef.addEventListener('click', onBtnClick);
+
+function onBtnClick() { 
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  colorNameRef.textContent = bodyRef.style.backgroundColor;
 }
